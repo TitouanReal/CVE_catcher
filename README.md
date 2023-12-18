4cccb60de119a7a86b3621877747bd31c7b755bd
Titouan Real <titouan.real@gmail.com>
2023-12-18 14:30:28 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CVE catcher
+
+
+## API used :
+
+https://nvd.nist.gov/developers/vulnerabilities
+
+## Project :
+
+Android application coded in Kotlin that can help with keeping track of new published CVE to keep servers protected
+
+## Devs :
+
+Quentin Daval & Titouan Real
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Catcher of Vulnerabilities and Exploits"
+include(":app")
